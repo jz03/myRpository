0566a0d3c1d55fe43910c8caa3893843557c87b2
jizhou <jizhou03@gmail.com>
2021-11-04 13:02:12 +0800

    add 添加新内容

--- 百度热搜/热搜常用SQL.sql
+++ 百度热搜/热搜常用SQL.sql
@@ -2,13 +2,13 @@
 select id,query,`desc`,create_date from hot_info ORDER BY	id DESC
 
 -- 抓取查询
-SELECT	branch_id,create_date FROM	hot_branch 
+SELECT	branch_id,create_date FROM	hot_branch
 GROUP BY	branch_id ORDER BY	branch_id DESC
 -- 综合消息
 select b.id,`index`,hot_score,`query`,`desc`,a.create_date,b.create_date as info_date from hot_branch a,hot_info b 
-where a.hot_info_id=b.id and a.branch_id='1635946060527'
+where a.hot_info_id=b.id and a.branch_id='1636001580454'
 
 select b.id,`index`,hot_score,`query`,`desc`,a.create_date from hot_branch a,hot_info b 
-where a.hot_info_id=b.id and b.query like '%郑州一小学4人检出阳性%'
+where a.hot_info_id=b.id and b.query like '%31省区市新增本土确诊%' ORDER BY	create_date DESC
 
 
