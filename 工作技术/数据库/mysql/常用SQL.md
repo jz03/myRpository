97cc75ba7ede79c189639795559bf20323c5de73
jizhou <jizhou03@gmail.com>
2021-11-06 09:47:19 +0800

    add 添加新内容

--- 工作技术/数据库/mysql/常用SQL.md
+++ 工作技术/数据库/mysql/常用SQL.md
@@ -1,7 +1,7 @@
 ### 1.查询表结构
 
 ```sql
-select COLUMN_NAME,DATA_TYPE,COLUMN_COMMENT from information_schema.columns where TABLE_NAME='表名'
+select column_name,is_nullable,column_type,column_key,extra,column_comment from information_schema.columns where TABLE_NAME='表名'
 ```
 
 ### 2.删除表中的数据并重置id自增
--- 百度热搜/热搜常用SQL.sql
+++ 百度热搜/热搜常用SQL.sql
@@ -6,7 +6,7 @@ SELECT	branch_id,create_date FROM	hot_branch
 GROUP BY	branch_id ORDER BY	branch_id DESC
 -- 综合消息
 select b.id,`index`,hot_score,`query`,`desc`,a.create_date,b.create_date as info_date from hot_branch a,hot_info b 
-where a.hot_info_id=b.id and a.branch_id='1636001580454'
+where a.hot_info_id=b.id and a.branch_id='1636019100203'
 
 select b.id,`index`,hot_score,`query`,`desc`,a.create_date from hot_branch a,hot_info b 
 where a.hot_info_id=b.id and b.query like '%31省区市新增本土确诊%' ORDER BY	create_date DESC
